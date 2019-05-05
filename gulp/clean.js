1a14cd327878061a6f703e3bb360f328b6849c66
Jason Lenoble <jason.lenoble@gmail.com>
2019-05-05 15:40:57 +0200

    Applied generator wupjs 0.6.0-0

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,8 +1,8 @@
-import gulp from 'gulp';
-import del from 'del';
+import {task} from "gulp";
+import del from "del";
 
 export const clean = () => {
-  return del('build');
+  return del("build");
 };
 
-gulp.task('clean', clean);
+task("clean", clean);
